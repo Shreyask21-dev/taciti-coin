f86615c6ad9cb624a240ca1caee4bb527fcf2396
vatsalya-sagar <vatsalyasagar8@gmail.com>
2024-10-24 15:42:03 +0530

    chages by sss

--- pages/[pageslug].js
+++ pages/[pageslug].js
@@ -8,57 +8,6 @@ export default function AllPages({ AllsinglePageList }) {
   const [loading, setLoading] = useState(true);
   const router = useRouter(); // Initialize useRouter
 
-  useEffect(() => {
-    // Timer to ensure loader is shown for a minimum duration
-    const timer = setTimeout(() => {
-      setLoading(false); // Set loading to false after the timer
-    }, 1000); // Minimum 2 seconds
-
-    // Cleanup function to clear the timer
-    return () => clearTimeout(timer);
-  }, [router.asPath]); // Run once on mount
-
-  // Show loader while loading
-  if (loading) {
-    return (
-      <div className="loader">
-        <style jsx>{`
-          .loader {
-            position: fixed;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background-color: rgba(255, 255, 255, 0.8);
-            z-index: 9999;
-          }
-
-          .loader::after {
-            content: "";
-            border: 8px solid rgba(255, 255, 255, 0.3);
-            border-radius: 50%;
-            border-top: 8px solid #3498db; /* Change color here */
-            width: 50px;
-            height: 50px;
-            animation: spin 1s linear infinite;
-          }
-
-          @keyframes spin {
-            0% {
-              transform: rotate(0deg);
-            }
-            100% {
-              transform: rotate(360deg);
-            }
-          }
-        `}</style>
-        <h2>Loading...</h2>
-      </div>
-    );
-  }
 
   return (
     <>
